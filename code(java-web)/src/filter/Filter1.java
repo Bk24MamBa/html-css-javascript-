81219474a24cad7a6c23794c20781f2092ab8ac9
Bk24Mamba <825251514@qq.com>
2019-08-12 17:45:42 +0800

    8.12

---
+++ code(java-web)/src/filter/Filter1.java
@@ -0,0 +1,34 @@
+package filter;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class Filter1 implements javax.servlet.Filter{
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		System.out.println("destory...");
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		System.out.println("¿πΩÿ«Î«Û...");
+		chain.doFilter(request, response);
+		System.out.println("¿πΩÿœÏ”¶...");
+	}
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		// TODO Auto-generated method stub
+		System.out.println("init...");
+	}
+
+}
