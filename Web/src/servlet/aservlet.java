594dc33d76c4f45ba84f716f8a4d819387b39be4
Bk24Mamba <825251514@qq.com>
2019-08-14 16:06:31 +0800

    8.14

---
+++ Web/src/servlet/aservlet.java
@@ -0,0 +1,14 @@
+package servlet;
+
+import java.io.IOException;
+
+@javax.servlet.annotation.WebServlet("/aservlet")
+public class aservlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        System.out.println("dopost...");
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        System.out.println("doget111...");
+    }
+}
---
+++ Web/web/index.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Bk MamBa
+  Date: 2019/8/14
+  Time: 15:01
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>8.14--IDEA</title>
+  </head>
+  <body>
+        Hello IDEA!112
+        <a href="aservlet">servlet</a>
+  </body>
+</html>
